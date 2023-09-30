fd053cb21b47aa150a185be095c4d7f0494bfdf6
brucelsprouts <134631582+brucelsprouts@users.noreply.github.com>
2023-09-29 20:12:00 -0400

    qaewe

--- lab 2/question 2.py
+++ lab 2/question 2.py
@@ -5,10 +5,10 @@ housePrice = int(input("Please enter the price of the hosue: "))
 
 if userScore >= IDEAL_CREDIT_SCORE: #Change => to >= (syntax error)
     downPayment = 0.1 * housePrice
-elif userScore < IDEAL_CREDIT_SCORE and userScore > "600": #Changes else if to elif (syntax error)
-    downPayment = 0.2 * housePrice
+elif userScore < IDEAL_CREDIT_SCORE and userScore > 600: #Changes else if to elif and remove
+    downPayment = 0.2 * housePrice                       #quotes around 600 (syntax and logic error)
 else:
-    downPayment = 0.3 * housePrice #Moved line to under the else staement (indentation error)
+    downPayment = 0.3 * housePrice #Indent line to under the else staement (indentation error)
 
 print("Your down payment is: ${}".format(downPayment)) #Replaced "()" with "{}" (logic error)
 
