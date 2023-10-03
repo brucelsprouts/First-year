c8faff522273efdca01a04c2f7d9b740f148dc00
brucelsprouts <134631582+brucelsprouts@users.noreply.github.com>
2023-10-03 13:33:22 -0400

    wad

--- labs/lab 3/question 2.py
+++ labs/lab 3/question 2.py
@@ -1,13 +1,12 @@
-# Initialize the accountTotal variable with $50
+#Initialize accountTotal to 50
 accountTotal = 50
 
-#Loops from 50 to 20
-while accountTotal > 20: #Changed =< to > since >= would make the balance go down to 19 at the end of the program
-    #Prints current account total
+#Use the correct comparison operator ">"" to run the loop until accountTotal is greater than 20
+while accountTotal > 20:
+    #Prints the current accountTotal
     print(accountTotal)
-
-    #Decrements accountTotal by 1$ every print
+    #Decriment accountTotal by one every run
     accountTotal -= 1
 
-#Print message when account has reached 20$
+#Print message shwoign account has reached $20
 print('Your account has reached $20.')
