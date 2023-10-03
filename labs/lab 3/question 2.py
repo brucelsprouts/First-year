6dafbf7006e87fcbcbc9e38ecefae95ef178ad73
brucelsprouts <134631582+brucelsprouts@users.noreply.github.com>
2023-10-03 13:39:19 -0400

    dw

--- labs/lab 3/question 2.py
+++ labs/lab 3/question 2.py
@@ -1,12 +1,32 @@
-#Initialize accountTotal to 50
-accountTotal = 50
+#Prompt the user to enter the number of values they want to use
+n = int(input("How many numbers do you want to use today? "))
 
-#Use the correct comparison operator ">"" to run the loop until accountTotal is greater than 20
-while accountTotal > 20:
-    #Prints the current accountTotal
-    print(accountTotal)
-    #Decriment accountTotal by one every run
-    accountTotal -= 1
+if n > 0:
+    #Prompt the user to enter the first number
+    firstValue = int(input("Enter the first number: "))
 
-#Print message shwoign account has reached $20
-print('Your account has reached $20.')
+    #Initialize variables for tracking largest, smallest, and total
+    largest = firstValue
+    smallest = firstValue
+    total = firstValue
+
+    counter = 0
+    #Loop to input and process the remaining numbers
+    while counter < (n - 1):
+        current = int(input("Enter the next number: "))
+        total = total + current
+        counter = counter + 1
+
+        if current < smallest:  #Find minimum
+            smallest = current        
+        elif current > largest: #Find maximum
+            largest = current
+
+    #Calculate and print the values
+    print("The average of the values is: ", total / n)
+    print("The smallest of the values is {}".format(smallest))
+    print("The largest of the values is {}".format(largest))
+    print("The range of the values is {}".format(largest - smallest))
+else:
+    #If the user entered a nonpositive number, inform them that no values will be used
+    print("You did not want to use any numbers today.")
