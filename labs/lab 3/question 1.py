5f5a1cd3fe831cd494a8c2d17e5b283ed28c30dd
brucelsprouts <134631582+brucelsprouts@users.noreply.github.com>
2023-10-03 11:30:26 -0400

    lab 3

--- labs/lab 3/question 1.py
+++ labs/lab 3/question 1.py
@@ -1,25 +1,36 @@
+#Prompt the user to enter the number of values they want to use
 n = int(input("How many numbers do you want to use today?"))
 
 if n > 0:
-    firstValue = int(input("enter the first number: "))
+    #Prompt the user to enter the first number
+    firstValue = int(input("Enter the first number: "))
 
+    #Initialize variables for tracking largest, smallest, and total
     largest = firstValue
     smallest = firstValue
     total = firstValue
 
     counter = 0
-    while counter < (n-1):
+    #Loop to input and process the remaining numbers
+    while counter < (n - 1):
         current = int(input("Enter the next number: "))
         total = total + current
         counter = counter + 1
-        if current < smallest: #find minimum
+
+        #Check if the current number is smaller than the smallest
+        if current < smallest:
             smallest = current
-        elif current > largest: #find maximum
+        #Check if the current number is larger than the largest
+        elif current > largest:
             largest = current
 
-    print("the average of the values is: ", total/n)
-    print("the smallest of the values is {}".format(smallest))
-    print("the largest of the values is {}".format(largest))
-    print("the range of the values is {}".format(largest-smallest))
+    #Calculate and print the average of the values
+    print("The average of the values is: ", total / n)
+    #Print the smallest and largest values
+    print("The smallest of the values is {}".format(smallest))
+    print("The largest of the values is {}".format(largest))
+    #Calculate and print the range of the values
+    print("The range of the values is {}".format(largest - smallest))
 else:
+    #If the user entered a non-positive number, inform them that no values will be used
     print("You did not want to use any numbers today.")
--- labs/lab 3/question 2.py
+++ labs/lab 3/question 2.py
@@ -1,5 +1,13 @@
+# Initialize the accountTotal variable with $50
 accountTotal = 50
-while accountTotal =< 20:
+
+#Loops from 50 to 20
+while accountTotal > 20: #Changed =< to > since >= would make the balance go down to 19 at the end of the program
+    #Prints current account total
     print(accountTotal)
 
+    #Decrements accountTotal by 1$ every print
+    accountTotal -= 1
+
+#Print message when account has reached 20$
 print('Your account has reached $20.')
