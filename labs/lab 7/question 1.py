9d89f54d3956417980f2a4767adcc45c01f7b752
brucelsprouts <134631582+brucelsprouts@users.noreply.github.com>
2023-11-08 13:04:08 -0500

    d

--- labs/lab 7/question 1.py
+++ labs/lab 7/question 1.py
@@ -1,6 +1,6 @@
-values = [1,2,3,4,5,"hello",6,7,8,9,"10"]
+values = [1, 2, 3, 4, 5, "hello", 6, 7, 8, 9, "10"]
 
 for cur in values:
-    print("The value is :", values[cur])
-    if type(values[cur]) == str:
-        print("This is a string!")
+    print("The value is:", cur)
+    if type(cur) == str:
+        raise ValueError("This is a string!")
