e4db71ae9b855ef41d467af579617ac9abc66c09
brucelsprouts <134631582+brucelsprouts@users.noreply.github.com>
2023-11-16 17:13:06 -0500

    asd

--- assignments/assignment 3/main.py
+++ assignments/assignment 3/main.py
@@ -11,20 +11,21 @@ This file takes inputs from the user and call the functions in sentiment_analysi
 
 # Import the sentiment_analysis module
 from sentiment_analysis import *
+import traceback
+
 
 # Main function to take inputs
 def main():
     try:
         keyword_file_name = input("Input keyword filename (.tsv file): ")
-        tweet_file_name = input("Input tweet filename (.csv file): ")
-        report_file_name = input("Input filename to output report in (.txt file): ")
-
         if not keyword_file_name.endswith('.tsv'):
             raise Exception("Must have tsv file extension!")
 
+        tweet_file_name = input("Input tweet filename (.csv file): ")
         if not tweet_file_name.endswith('.csv'):
             raise Exception("Must have csv file extension!")
 
+        report_file_name = input("Input filename to output report in (.txt file): ")
         if not report_file_name.endswith('.txt'):
             raise Exception("Must have txt file extension!")
 
@@ -33,12 +34,12 @@ def main():
 
         if not keyword_dict or not tweet_list:
             raise Exception("Tweet list or keyword dictionary is empty!")
-            
-        report = make_report(tweet_list, keyword_dict)
 
+        report = make_report(tweet_list, keyword_dict)
         write_report(report, report_file_name)
 
     except Exception as e:
-        print(e)
+        tb = traceback.format_exc()
+        print(f"{tb}")
 
 main()
